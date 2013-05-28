304df17554f3a50faf9dbc94a8372389adede8fe
Damian Rusinek <damian.rusinek@gmail.com>
2013-05-28 15:20:46 +0200

    Reduction: Started algorithm

--- qopml/interpreter/simulator/expression.py
+++ qopml/interpreter/simulator/expression.py
@@ -3,8 +3,8 @@ Created on 07-05-2013
 
 @author: Damian Rusinek <damian.rusinek@gmail.com>
 '''
-from qopml.interpreter.model import IdentifierExpression, CallFunctionExpression,\
-    TupleExpression
+import copy
+from qopml.interpreter.model import IdentifierExpression, CallFunctionExpression, TupleExpression
 from qopml.interpreter.simulator.error import RuntimeException
 
 class Checker():
@@ -44,5 +44,55 @@ class Reducer():
     def __init__(self, equations):
         self.equations = equations
         
+    def _get_reduction_points(self, expression):
+        raise NotImplementedError()
+        
     def reduce(self, expression):
-        pass
+        """
+        Reduces expression with usage of equations.
+        Returns True if expression is reduced or False if it is not reducable.
+        Raises exception if ambiguity is found.
+        """
+        
+        reduced = False
+        continue_reducing = True
+        
+        # Reduce until no reduction can be performed.
+        # One reduction can give way for another reduction.
+        while continue_reducing:
+            continue_reducing = False
+            
+            # For each equation we find points where expression can be reduced
+            reduction_points = self._get_reduction_points(expression)
+        
+            if len(reduction_points) > 0:
+                
+                # For each poing:
+                #  - temporary reduce at this point
+                #  - remove used point from reduction points list
+                #  - generate new reduction points list for reduced expression
+                #  - if any of points from old list is not present in new list raise ambiguity exception
+                #  ! New reduction points may come
+                
+                for reduction_point in reduction_points:
+                    
+                    # Generate list with reduction points before reduction
+                    old_reduction_points = copy.copy(reduction_points)
+                    old_reduction_points.remove(reduction_point)
+                    
+                    # Reduce temporary
+                    reduction_point.reduce()
+                    
+                    # Generate new reduction points
+                    new_reduction_points = self._get_reduction_points(expression)
+                    
+                    
+                    
+                     
+                
+                
+                
+                
+                
+                
+                
