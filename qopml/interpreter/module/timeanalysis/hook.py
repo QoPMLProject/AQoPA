da1b6b057692f24db34630ce6f3e79d9f67a91ae
Damian Rusinek <damian.rusinek@gmail.com>
2013-06-24 06:01:06 +0200

    Added communication synchronization in time analysis and changed executions

--- qopml/interpreter/module/timeanalysis/hook.py
+++ qopml/interpreter/module/timeanalysis/hook.py
@@ -4,7 +4,7 @@ Created on 31-05-2013
 @author: Damian Rusinek <damian.rusinek@gmail.com>
 '''
 import random
-from qopml.interpreter.simulator.state import Hook
+from qopml.interpreter.simulator.state import Hook, ExecutionResult
 from qopml.interpreter.model import AssignmentInstruction,\
     CallFunctionInstruction, IfInstruction, WhileInstruction,\
     CommunicationInstruction, CallFunctionExpression, TupleExpression
@@ -25,8 +25,23 @@ class PrintResultsHook(Hook):
             if h.get_finish_error():
                 sys.stdout.write('\t! Finished with error: %s' % h.get_finish_error())
             sys.stdout.write("\n")
+            
+        print ""
+        print "Dropped messages:"
+        i = 0
+        for c in context.channels_manager.channels:
+            if c.get_number_of_dropped_messages() > 0:
+                i += 1
+                print '%s \t %d' % (c.name, c.get_number_of_dropped_messages()) 
+                
+        if i == 0:
+            print "None"
                 
 class PreInstructionHook(Hook):
+    """
+    Execution hook executed before default core execution of each instruction.
+    Returns execution result.
+    """
     
     def __init__(self, module):
         """ """
@@ -43,9 +58,10 @@ class PreInstructionHook(Hook):
             return
         
         if isinstance(instruction, CommunicationInstruction):
-            self._execute_communication_instruction(context)
+            return self._execute_communication_instruction(context)
         else:
             self._update_time(context)
+            return ExecutionResult()
             
     def _update_time(self, context):
         """ 
@@ -162,14 +178,56 @@ class PreInstructionHook(Hook):
         """ """
         channel = context.channels_manager.find_channel_for_current_instruction(context)
         if not channel:
-            return
+            return None
         
         instruction = context.get_current_instruction()
         expressions_cnt = len(instruction.variables_names)
         
+        # Check if other hosts should send their message 
+        # through this channel before
+        current_host_time = self.module.get_current_time(context.get_current_host())
+        delay_communication_execution = False
+        for h in context.hosts:
+            # Omit finished hosts
+            if h.finished():
+                continue
+            # Omit current host
+            if h == context.get_current_host():
+                continue
+            # Omit hosts not using current channel 
+            if not channel.connected_with_host(h):
+                continue
+            
+            # Omit hosts that are waiting for the message on current channel
+            current_instruction = h.get_current_instructions_context()\
+                                    .get_current_instruction()
+            if isinstance(current_instruction, CommunicationInstruction):
+                
+                ch = None
+                if instruction.is_out(): # OUT instruction
+                    if not current_instruction.is_out(): # IN instruction
+                        ch = context.channels_manager.find_channel_for_host_instruction(
+                                                        context, h, current_instruction)
+                else: # IN instruction
+                    if current_instruction.is_out(): # OUT instruction
+                        ch = context.channels_manager.find_channel_for_host_instruction(
+                                                        context, h, current_instruction)
+                if ch == channel:
+                    continue
+            
+            if self.module.get_current_time(h) < current_host_time:
+                delay_communication_execution = True
+                
+        # Delay execution of this instruction 
+        # if needed according to previous check      
+        if delay_communication_execution:
+            return ExecutionResult(custom_index_management=True, 
+                                   finish_instruction_execution=True)
+                
+                
         if instruction.is_out():
             sender = context.get_current_host()
-            receiver = None
+              
             
             receivers_list = channel.get_queue_of_receiving_hosts(expressions_cnt)
             for i in range(0, len(receivers_list)):
@@ -179,10 +237,9 @@ class PreInstructionHook(Hook):
                                                           sender,
                                                           self.module.get_current_time(sender))
                 else:
-                    if self.module.get_current_time(sender) < self.module.get_current_time(receiver):
-                        raise TimeSynchronizationException("Time synchronization error. " + \
-                                                           "Trying to send message from host '%s' at time %s ms " + \
-                                                           "while receiving host '%s' has time %s ms." % 
+                    if self.module.get_current_time(sender) < self.module.get_current_time(receivers_list[i]):
+                        
+                        raise TimeSynchronizationException("Time synchronization error. Trying to send message from host '%s' at time %s ms while receiving host '%s' has time %s ms." % 
                                                            (unicode(sender),
                                                             self.module.get_current_time(sender),
                                                             unicode(receivers_list[i]),
