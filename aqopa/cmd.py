947837866329701e1f367f69be2ffdac4db9ce1b
Damian Rusinek <damian.rusinek@gmail.com>
2013-10-31 16:23:18 +0100

    Added library functionality.

--- aqopa/cmd.py
+++ aqopa/cmd.py
@@ -8,8 +8,6 @@ import optparse
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
 from aqopa import VERSION
 from aqopa.bin import console, gui
 
---
+++ bin/aqopa-console.py
@@ -0,0 +1,12 @@
+'''
+Created on 31-10-2013
+
+@author: Damian Rusinek <damian.rusinek@gmail.com>
+'''
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from aqopa import cmd
+cmd.console_command()
