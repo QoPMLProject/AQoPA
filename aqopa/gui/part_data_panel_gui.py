62d09e58a6e77a325075085bbd0a61da9dbb70d1
Katarzyna Mazur <kasiula.mazur@gmail.com>
2014-05-07 13:06:08 +0200

    Changed the GUI layout on MMV tabz

--- aqopa/gui/part_data_panel_gui.py
+++ aqopa/gui/part_data_panel_gui.py
@@ -4,7 +4,7 @@
 @file       part_data_panel_gui.py
 @brief      panel for model, metrics and versions tabs on AQoPA's main window
 @author     Damian Rusinek
-@author     Katarzyna Mazur (visual improvements)
+@author     Katarzyna Mazur (visual improvements mainly)
 @date       created on 05-09-2013 by Damian Rusinek
 @date       edited on 06-05-2014 by Katarzyna Mazur
 """
@@ -20,7 +20,10 @@ class ModelPartDataPanel(wx.Panel):
     """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+         @brief Initializes and aligns all the gui elements for
+         tabs: model, metrics and versions
+        """
         wx.Panel.__init__(self, *args, **kwargs)
 
         # create group boxes aka static boxes
@@ -33,70 +36,55 @@ class ModelPartDataPanel(wx.Panel):
         self.dataTextArea = wx.richtext.RichTextCtrl(self, style=wx.TE_MULTILINE | wx.TE_NO_VSCROLL)
 
         # create buttons - simple 'Load' and 'Save' will be enough
+        self.loadButton = wx.Button(self, label="Load")
+        self.saveButton = wx.Button(self, label="Save")
 
+        # create label 4 displaying information about the cursor
+        # position in the opened file
+        self.cursorInfoLabel = wx.StaticText(self)
+
+        # create checkbox - make model/metric/version editable / not editable
+        self.editable = wx.CheckBox(self, -1, label="Editable")
+
+        # do some bindings -
+        # show cursor position below the text area
+        self.dataTextArea.Bind(wx.EVT_KEY_UP, self.printCursorInfo)
+        self.dataTextArea.Bind(wx.EVT_LEFT_UP, self.printCursorInfo)
+        # bind buttons to appropriate actions
+        self.loadButton.Bind(wx.EVT_BUTTON, self.OnLoadClicked)
+        self.saveButton.Bind(wx.EVT_BUTTON, self.OnSaveClicked)
+        # bind checkbox state with the appropriate action - simply
+        # allow / do not allow to edit opened model/metric/version
+        self.editable.Bind(wx.EVT_CHECKBOX, self.OnCheckBoxClicked)
+
+        # at first, we are not allowed to edit
+        self.editable.SetValue(False)
+        # pretend that we clicked the check box, so it's event gets called
+        wx.PostEvent(self.editable, wx.CommandEvent(wx.wxEVT_COMMAND_CHECKBOX_CLICKED))
+
+        # create empty static box (label) in order to make a horizontal
+        # gap between the checkbox and the buttons
+        __gap = wx.StaticText(self)
+
+        # align buttons and the checkbox
+        bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
+        bottomSizer.Add(self.editable, 0, wx.EXPAND | wx.ALIGN_LEFT, 5)
+        bottomSizer.Add(__gap, 1, wx.EXPAND, 5)
+        bottomSizer.Add(self.loadButton, 0, wx.EXPAND | wx.ALIGN_RIGHT, 5)
+        bottomSizer.Add(self.saveButton, 0, wx.EXPAND | wx.ALIGN_RIGHT, 5)
 
         # add text area to the fancy group box with a filename above the displayed file content
         self.tabBoxSizer.Add(self.dataTextArea, 1, wx.EXPAND | wx.ALL, 5)
 
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # do the final alignment
+        sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.tabBoxSizer, 1, wx.EXPAND | wx.ALL, 5)
-
+        sizer.Add(self.cursorInfoLabel, 0, wx.EXPAND | wx.ALL, 5)
+        sizer.Add(bottomSizer, 0, wx.ALIGN_RIGHT | wx.RIGHT, 5)
         self.SetSizer(sizer)
         self.Layout()
 
-        """bPanel = wx.Panel(self)
-        bSizer = wx.BoxSizer(wx.VERTICAL)
-        bPanel.SetSizer(bSizer)
-
-        self.loadButton = wx.Button(bPanel)
-        self.saveButton = wx.Button(bPanel)
-
-        self.attachButtons(self.loadButton, self.saveButton)
-
-        bSizer.Add(self.loadButton, 0, wx.ALL, 5)
-        bSizer.Add(self.saveButton, 0, wx.ALL, 5)
-
-        rightPanel = wx.Panel(self)
-        rightPanelSizer = wx.BoxSizer(wx.VERTICAL)
-        rightPanel.SetSizer(rightPanelSizer)
-
-        self.dataTextArea = wx.richtext.RichTextCtrl(rightPanel, style=wx.TE_MULTILINE | wx.TE_NO_VSCROLL)
-        self.dataTextArea.Bind(wx.EVT_KEY_UP, self.printCursorInfo)
-        self.dataTextArea.Bind(wx.EVT_LEFT_UP, self.printCursorInfo)
-
-        self.cursorInfoLabel = wx.StaticText(rightPanel, label="")
-
-        rightPanelSizer.Add(self.dataTextArea, 1, wx.EXPAND)
-        rightPanelSizer.Add(self.cursorInfoLabel, 0, wx.ALIGN_RIGHT)
-
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(bPanel, 0, wx.ALL, 5)
-        sizer.Add(rightPanel, 1, wx.ALL | wx.EXPAND, 5)
-
-        # fill panel with linear gradient to make it look fancy
-        self.Bind(wx.EVT_PAINT, self.OnPaintPrettyPanel)
-
-        self.SetSizer(sizer)
-
-        self.Layout()"""
-
-    def OnPaintPrettyPanel(self, event):
-        # establish the painting canvas
-        dc = wx.PaintDC(self)
-        x = 0
-        y = 0
-        w, h = self.GetSize()
-        dc.GradientFillLinear((x, y, w, h), '#606060', '#E0E0E0', nDirection=wx.NORTH)
-
-    def SetOpenedFilename(self, filename):
-        self.tabBox.SetLabel(filename)
-
-    def attachButtons(self, loadButton, saveButton):
-        """ """
-        loadButton.Bind(wx.EVT_BUTTON, self.onLoad)
-        saveButton.Bind(wx.EVT_BUTTON, self.onSave)
-
-    def onLoad(self, event):
+    def OnLoadClicked(self, event):
         """ Load file to text area """
         ofdlg = wx.FileDialog(self, "Load file", "", "", "QoP-ML Files (*.qopml)|*.qopml",
                               wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
@@ -105,18 +93,22 @@ class ModelPartDataPanel(wx.Panel):
             wildcard, types = wx.richtext.RichTextBuffer.GetExtWildcard(save=False)
             fileType = types[ofdlg.GetFilterIndex()]
             self.dataTextArea.LoadFile(ofdlg.GetPath(), fileType)
-        self.SetOpenedFilename(ofdlg.GetFilename())
+        self.SetFilenameOnGUI(ofdlg.GetPath())
         ofdlg.Destroy()
 
-    def onSave(self, event):
+    def OnSaveClicked(self, event):
         """ Save text area value to file """
         ofdlg = wx.FileDialog(self, "Save file", "", "", "QoP-ML Files (*.qopml)|*.qopml",
                               wx.FD_SAVE)
         ofdlg.ShowModal()
-        if ofdlg.GetPath():
-            f = open(ofdlg.GetPath(), "w")
+        if ofdlg.GetPath() :
+            # add *.qopml extension if not given
+            validated = self.ValidateFilename(ofdlg.GetPath())
+            f = open(validated, "w")
             f.write(self.dataTextArea.GetValue())
             f.close()
+        # save on gui name of the saved file (with *.qopml) extension
+        self.SetFilenameOnGUI(validated)
         ofdlg.Destroy()
 
     def printCursorInfo(self, event):
@@ -127,3 +119,41 @@ class ModelPartDataPanel(wx.Panel):
                                       % (xy[1]+1, xy[0]+1))
         self.Layout()
         event.Skip()
+
+    def OnPaintPrettyPanel(self, event):
+        # establish the painting canvas
+        dc = wx.PaintDC(self)
+        x = 0
+        y = 0
+        w, h = self.GetSize()
+        dc.GradientFillLinear((x, y, w, h), '#606060', '#E0E0E0', nDirection=wx.NORTH)
+
+    def SetFilenameOnGUI(self, filename):
+        """
+        @brief      sets the title of the group box
+                    to the opened/saved filename
+        """
+        self.tabBox.SetLabel("File: "+filename)
+
+    def ValidateFilename(self, filename):
+        """
+        @brief      adds *.qopml extension to the file which
+                    we want 2 save (but only if not given)
+        @return     returns the validated filename
+        """
+        if filename.endswith(".qopml") :
+            return filename
+        else :
+            filename += ".qopml"
+            return filename
+
+    def OnCheckBoxClicked(self, event):
+        """
+        @brief      checks if the checkbox is checked :D
+                    if so, you can edit opened model/metric/version
+                    if not, model/metric/version is not editable
+        """
+        if event.IsChecked() :
+            self.dataTextArea.SetEditable(True)
+        else :
+            self.dataTextArea.SetEditable(False)
