27537c02a4fb7713f1d6961069d0bf94056534cb
Katarzyna Mazur <kasiula.mazur@gmail.com>
2014-08-01 16:01:11 +0200

    Occured facts finally gathered within the QoP module.

--- aqopa/module/qop/hook.py
+++ aqopa/module/qop/hook.py
@@ -29,12 +29,12 @@ class PreInstructionHook(Hook):
         instruction = context.get_current_instruction()
 
         # temp - test me!
-        if instruction.__class__ is CallFunctionInstruction:
-            print "function_name: "+str(instruction.function_name)
-            print "arguments: "+str(instruction.arguments)
-            print "qop_arguments: "+str(instruction.qop_arguments)
-            for i in instruction.arguments:
-                print "qop_argument:"+str(i.identifier)
+        # if instruction.__class__ is CallFunctionInstruction:
+        #     print "function_name: "+str(instruction.function_name)
+        #     print "arguments: "+str(instruction.arguments)
+        #     print "qop_arguments: "+str(instruction.qop_arguments)
+        #     for i in instruction.arguments:
+        #         print "qop_argument:"+str(i.identifier)
 
         if instruction.__class__ not in [AssignmentInstruction, CallFunctionInstruction,
                                      IfInstruction, WhileInstruction]:
@@ -59,6 +59,7 @@ class PreInstructionHook(Hook):
         if isinstance(instruction, AssignmentInstruction):
             expression = instruction.expression
         elif isinstance(instruction, CallFunctionInstruction):
+            print "instruction.qop_arguments = " + str(instruction.qop_arguments)
             expression = CallFunctionExpression(instruction.function_name, instruction.arguments, instruction.qop_arguments)
         else:
             expression = instruction.condition
@@ -71,14 +72,13 @@ class PreInstructionHook(Hook):
         self.module.add_occured_fact(self.simulator, context.get_current_host(), fact)
 
     def _get_all_facts_details_for_expression(self, context, expression):
+
         if isinstance(expression, TupleExpression):
             return self._get_all_facts_details_for_tuple_expression(context, expression)
         elif isinstance(expression, CallFunctionExpression):
             return self._get_all_facts_details_for_simple_expression(context, expression)
         elif isinstance(expression, ComparisonExpression):
             return self._get_all_facts_details_for_comparison_expression(context, expression)
-        elif isinstance(expression, IdentifierExpression):
-            return self._get_all_facts_details_for_identifier_expression(context, expression)
         return []
 
     def _get_all_facts_details_for_tuple_expression(self, context, expression):
@@ -93,14 +93,9 @@ class PreInstructionHook(Hook):
 
     def _get_all_facts_details_for_simple_expression(self, context, expression):
         qop_args = []
-        #for expr in expression.arguments:
         for expr in expression.qop_arguments:
-            print "expr = " + str(expr)
             e = self._get_all_facts_details_for_expression(context, expr)
-            if type(e) is list :
-                qop_args.extend(e)
-            else:
-                qop_args.append(e)
+            qop_args.append(str(expr))
         return qop_args
 
     def _get_all_facts_details_for_comparison_expression(self, context, expression):
