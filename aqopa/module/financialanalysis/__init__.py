83dd9d78fa5844e1199c315954cf695725f7deb7
Katarzyna <kasiula.mazur@gmail.com>
2014-09-10 22:30:15 +0200

    Improvements

--- aqopa/module/financialanalysis/__init__.py
+++ aqopa/module/financialanalysis/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from aqopa import module
-from aqopa import module
 from aqopa.simulator.state import HOOK_TYPE_SIMULATION_FINISHED, HOOK_TYPE_PRE_INSTRUCTION_EXECUTION
 
 """
@@ -12,9 +11,14 @@ from aqopa.simulator.state import HOOK_TYPE_SIMULATION_FINISHED, HOOK_TYPE_PRE_I
 
 class Module(module.Module):
 
-    def __init__(self):
+    def __init__(self) :
+        pass
 
-        self.watts = 0
-        self.usage_time = 0
-        self.price_per_kWh = 0
-        self.final_cost = 0
+    def calculate(self):
+        """
+        @brief calculates the total cost ($) of the host
+        usage, its quite simple math:
+        1) Watts / 1000 = X kWatts
+        2) X kWatts * Y hours = Z kWh
+        3) $/kWh * Z kWh = final cost
+        """
--- aqopa/module/financialanalysis/finance.py
+++ aqopa/module/financialanalysis/finance.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python
+
+"""
+@file       finance.py
+@brief      initial file for the financialanalysis module
+@author     Katarzyna Mazur
+"""
+
+class Finance() :
+
+    def __init__(self, watts=0, usage_time=0, price_per_kWh=0, final_cost = 0) :
+        self.watts = watts
+        self.usage_time = usage_time
+        self.price_per_kWh = price_per_kWh
+        self.final_cost = final_cost
+
